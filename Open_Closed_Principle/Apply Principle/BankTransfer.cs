ea2fd7f5fb37951fccc7b5392e12c1aa25dfeaa0
Yosif Taha <ywsfwhab314@gmail.com>
2026-03-15 01:56:06 +0200

    An example demonstrating the Open-Closed Principle by designing classes that can be extended with new functionality without modifying existing code.

---
+++ Open_Closed_Principle/Apply Principle/BankTransfer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed_Principle.Apply_Principle
+{
+    internal class BankTransfer : IPaymentProcessor
+    {
+        public void Pay()
+        {
+            // Process bank transfer payment
+        }
+    
+    }
+}
---
+++ Open_Closed_Principle/Problem/PaymentProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Closed_Principle.Problem
+{
+    public class PaymentProcessor
+    {
+        void Pay(string paymentType)
+        {
+            if (paymentType == "CreditCard")
+            {
+                // Process credit card payment
+            }
+            else if (paymentType == "PayPal")
+            {
+                // Process PayPal payment
+            }
+            else if (paymentType == "BankTransfer")
+            {
+                // Process bank transfer payment
+            }
+        }
+    }
+}
