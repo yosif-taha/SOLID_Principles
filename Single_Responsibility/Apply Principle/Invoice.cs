fc713aba9d746a114f71442c60126143f2a22ba6
Yosif Taha <ywsfwhab314@gmail.com>
2026-03-15 00:07:00 +0200

    Example implementation of the Single Responsibility Principle in C#.

---
+++ Single_Responsibility/Apply Principle/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility.Apply_Principle
+{
+    internal class Invoice
+    {
+        // Invoice Data and Properties
+
+        public decimal CalculateTotal()
+        {
+            // Code to calculate the total amount of the invoice
+            return 0; // Placeholder return value
+        }
+    }
+}
---
+++ Single_Responsibility/Apply Principle/InvoiceService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility.Apply_Principle
+{
+    internal class InvoiceService
+    {
+        public void SendEmail(Invoice invoice)
+        {
+            // Code to send the invoice via email to the customer
+        }
+    }
+}
